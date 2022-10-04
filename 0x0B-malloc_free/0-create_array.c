cab9fff30f03c136357a9251c0633e5e02f67142
thecrown4me <adeoyemanuwa@yahoo.com>
2022-10-04 04:02:51 -0700

    malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,8 +9,7 @@
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
-	char *s;
-	if (size == 0)
+	char *s;  if (size == 0)
 		return (NULL);
 	s = (char *)malloc(size * sizeof(char));
 	if (s == NULL)
